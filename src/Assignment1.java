83c44f5b09b28f1b153153038179c98aaff490af
moham2202 <mohamfaisal4@gmail.com>
2023-03-25 04:43:29 +0300

    assingment1

---
+++ src/Assignment1.java
@@ -0,0 +1,12 @@
+package Assignment1;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+    char c1= args[0].charAt(0);
+    char c2= args[1].charAt(0);
+    int offs=(int)c2-(int)c1;
+    System.out.println("Offset is "+offs); 		
+	}
+
+}
